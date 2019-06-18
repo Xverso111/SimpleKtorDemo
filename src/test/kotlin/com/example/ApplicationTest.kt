e553f0cc659bc0a629dba2d264c000ad5cf054ff
Pablo Correa <pcorrea@thoughtworks.com>
2019-06-18 16:01:18 -0500

    Simple unit test fixed

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -13,7 +13,7 @@ class ApplicationTest {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLDwwww!", response.content)
+                assertEquals("HELLO WORLD!", response.content)
             }
         }
     }
